5d7d49532a81d9329d0466de6cfb79b6a7d52af0
Rogerson Nazário <rrnazario@gmail.com>
2025-02-01 10:10:29 +0100

    chore: automatic year

--- src/components/StaticFooter/index.tsx
+++ src/components/StaticFooter/index.tsx
@@ -9,13 +9,13 @@ function StaticFooter({ spinLogo }: StaticFooterProps) {
     return <Styled.Container>
         <Logo spin={spinLogo} />
         <Styled.Detail>
-            <p>Diário de Rolê - 2024</p>
+            <p>{`Diário de Rolê - ${new Date().getFullYear()}`}</p>
             <p>Todos os direitos reservados</p>
         </Styled.Detail>
         <h3>...</h3>
 
         <Styled.DevFooter>
-            <p>2024 - Desenvolvido por <a href='https://linktr.ee/zariosoft' target='_blank' rel="noreferrer">ZarioSoft</a></p>
+            <p>{`${new Date().getFullYear()} - Desenvolvido por`} <a href='https://linktr.ee/zariosoft' target='_blank' rel="noreferrer">ZarioSoft</a></p>
         </Styled.DevFooter>
     </Styled.Container>
 }
